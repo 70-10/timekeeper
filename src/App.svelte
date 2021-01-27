adfaf95c46c9fd292d56005f51ba901e9544856d
70_10 <7010.dev@gmail.com>
2021-01-27 14:56:32 +0900

    Add Tailwind and logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,64 @@
 <script>
-  import { onMount } from "svelte";
-  export let date;
+  import numeral from "numeral";
 
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
-  });
+  let time = 60;
+  let max = 60;
+  let interval = null;
+
+  $: percentage = (time / max) * 100;
+  $: timeNumeral = numeral(time).format("00:00:00");
+
+  function start() {
+    interval = setInterval(() => {
+      time--;
+      if (time <= 0) {
+        clearInterval(interval);
+        return;
+      }
+    }, 1000);
+  }
+
+  function pose() {
+    clearInterval(interval);
+    interval = null;
+  }
+
+  const reset = () => {
+    if (interval) {
+      pose();
+    }
+    time = 60;
+    max = 60;
+  };
 </script>
 
-<main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
+<main
+  class="flex flex-col items-center justify-center h-screen"
+  style="background-image: linear-gradient(
+      to right,
+      rgb(92, 191, 148) 0% {percentage}%,
+      white {percentage}%
+    );"
+>
+  <span class="text-7xl md:text-9xl font-bold">{timeNumeral}</span>
+
+  {#if !interval}
+    <button on:click={start} class="border rounded-lg bg-white py-1 px-3"
+      >start</button
+    >
+  {:else}
+    <button on:click={pose} class="border rounded-lg bg-white py-1 px-3"
+      >Pose</button
+    >
+  {/if}
   <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <button on:click={reset} class="border rounded-lg bg-white py-1 px-3"
+    >Reset</button
+  >
 </main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
